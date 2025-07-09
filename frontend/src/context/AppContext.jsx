36c871fd48a7ab46cf25c45272c8e9febe9a798e
Abhishek Chauhan <abhishekchauhan01x@gmail.com>
2025-07-09 11:02:28 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -9,7 +9,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currenySymbol = '$'
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://medisync-kqhk.onrender.com"
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
