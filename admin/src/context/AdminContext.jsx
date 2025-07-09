eabbd022002fcb0fe91e4f993d27f348849a5918
Abhishek Chauhan <abhishekchauhan01x@gmail.com>
2025-07-09 10:58:58 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -13,7 +13,7 @@ const AdminContextProvider = (props) => {
     const [appointments, setAppointments] = useState([])
     const [dashData, setDashData] = useState(false)
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://medisync-kqhk.onrender.com"
 
     const getAllDoctors = async () => {
 
