36657dee9a276b9ffe1eddff85b49a4bb4e4ae90
Abhishek Chauhan <abhishekchauhan01x@gmail.com>
2025-07-10 09:52:42 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,7 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors({
-  origin: 'https://medisync-mu.vercel.app',
-  credentials: true,
-}))
+app.use(cors())
 
 // api endpoints
 app.use('/api/admin',adminRouter)
