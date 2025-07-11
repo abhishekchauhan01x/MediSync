e4b91c319aa6b8fbafefe974b5eb87a5a39d6c4c
Abhishek Chauhan <abhishekchauhan01x@gmail.com>
2025-07-11 13:51:00 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -13,17 +13,27 @@ const port = process.env.PORT || 3000
 connectDB()
 connectCloudinary()
 
+app.use(express.json())
 
 // middlewares
-app.use(express.json())
+const allowedOrigins = [
+    'https://medi-sync-dusky.vercel.app',
+    'https://medisync-admin-mu.vercel.app'
+];
+
 app.use(cors({
-    origin: [
-        'https://medi-sync-dusky.vercel.app',
-        'https://medisync-admin-mu.vercel.app'
-    ],
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
-}))
+}));
+
+app.options('*', cors()); // handles preflight OPTIONS requests
 
 // api endpoints
 app.use('/api/admin',adminRouter)
